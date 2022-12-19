6851cc18f12b35105053326f3cf8ffcbcd59c281
jennyffermorais <jennyffernovaes@hotmail.com>
2022-12-19 18:17:20 -0300

    Add some tests

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,19 @@
+import { Content } from './content';
+
+describe('Notification content', () => {
+  it('it should be able to create a notification content', () => {
+    const content = new Content(
+      'Você recebeu uma nova solicitação de amizade.',
+    );
+
+    expect(content).toBeTruthy();
+  });
+
+  it('it should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('oii')).toThrow();
+  });
+
+  it('it should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('o'.repeat(241))).toThrow();
+  });
+});
