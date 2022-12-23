3f876b977c273628544f6645b4abbec25fe5e8f2
jennyffermorais <jennyffernovaes@hotmail.com>
2022-12-23 14:47:34 -0300

    implements prisma's repository

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -16,8 +16,11 @@ export class Notification {
   private props: NotificationProps;
 
   // na hora de criar a notificação, o createdAt é opcional
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,6 +1,9 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
+  // converter a notificação da camada de app, para a notificação da camada do prisma
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -10,5 +13,20 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  // converter a notificação da camada do prisma, para a notificação da camada da app
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
